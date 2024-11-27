d7c9dc410d8e517cf42d07dc854990301a6050f1
zawn <zawn@berea.edu>
2024-11-27 10:20:11 -0500

    open cv learning

--- codebase/finalpassword/logic/CVE.py
+++ codebase/finalpassword/logic/CVE.py
@@ -1,14 +1,34 @@
+from flask import Flask
 import requests
 
-ip_address ="199.60.103.226"
+
+app = Flask(__name__)
+
+ip_address ="34.210.108.54"
 cve_check = f"https://internetdb.shodan.io/{ip_address}"
 cve_get = requests.get(cve_check)
+print(cve_get)
 cve_json = cve_get.json()
+print(cve_json)
 cve_vulns = cve_json["vulns"]
+print(cve_vulns)
 cve_id = cve_vulns[0]
 cve_db = f"https://cve.circl.lu/api/cve/{cve_id}"
 db_get = requests.get(cve_db)
 db_json = db_get.json()
+print(db_json)
+db_json = db_json["capec"]
+db_json = db_json[0]
+attack_name = db_json["name"]
+pre_req = db_json["prerequisites"]
+sol = db_json["solutions"]
+sum = db_json["summary"]
 
+@app.route('/')
+def hello_world():
+    return f'The following website has the following cyber vulnerability: {attack_name} \n\n prerequiste: {pre_req} \n\n solution: {sol} \n\n summary: {sum}' 
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
--- codebase/finalpassword/logic/facial.py
+++ codebase/finalpassword/logic/facial.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import sys
+import os
+import time
 
 camera = 0
 if len(sys.argv) > 1:
@@ -9,6 +11,23 @@ if len(sys.argv) > 1:
 source = cv.VideoCapture(camera)
 print(source)
 
+def face_capture(frame):
+    algorithm = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_id = input("enter id of user: ")
+    name = input("Enter name: ")
+    sample = int(input("Enter how many sample you wish to take: "))
+
+    for f in os.listdir(path):
+        os.remove(os.path.join(path, f))
+    time.sleep(2)
+
+    count=0
+    while True:
+        ret,img=frame.read()
+        converted_image=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        face=algorithm.detectMultiScale(converted_image,1.3,5)
+
+
 cv.namedWindow("Camera Pop-up", cv.WINDOW_NORMAL)
 while cv.waitKey(2) != 27:
     frame_exist, img_matrix = source.read()
@@ -16,6 +35,6 @@ while cv.waitKey(2) != 27:
         break
     else:
         cv.imshow("Camera Pop-up", img_matrix)
-        
+        face_capture(frame)
 
 
